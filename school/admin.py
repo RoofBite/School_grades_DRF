196e4c011fea6b56834658c6463b92b03b2b12ae
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-07-25 15:59:07 +0200

    Major changes in models

--- school/admin.py
+++ school/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import School, SchoolClass, SchoolSubject, Student, Teacher
 
+
 admin.site.register(School)
 admin.site.register(SchoolClass)
 admin.site.register(SchoolSubject)
--- school/models.py
+++ school/models.py
@@ -2,32 +2,56 @@ from django.db import models
 from django.db.models.fields.related import OneToOneField
 from django.contrib.auth.models import User
 
+class School(models.Model):
+    name = models.CharField(max_length = 100)
 
-
-class Student(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
-    user = OneToOneField(User, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     user = OneToOneField(User, on_delete = models.CASCADE)
+    school=models.ManyToManyField(School)
+
+    def __str__(self):
+        return self.first_name + self.last_name
 
 class SchoolSubject(models.Model):
     name = models.CharField(max_length = 50)
     teacher = models.ManyToManyField(Teacher)
-    student = models.ForeignKey(Student, on_delete = models.SET_NULL, null=True)
+    school=models.ForeignKey(School, on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
 
 class SchoolClass(models.Model):
     name = models.CharField(max_length = 50)
     supervising_teacher = models.OneToOneField(Teacher, null=True, on_delete = models.CASCADE)
-    member = models.ForeignKey(Student, on_delete = models.SET_NULL, null=True)
+    school=models.ForeignKey(School, on_delete = models.CASCADE, null=True)
     subject = models.ManyToManyField(SchoolSubject)
 
-class School(models.Model):
-    name = models.CharField(max_length = 100)
-    teacher = models.ManyToManyField(Teacher)
-    student = models.ForeignKey(Student, on_delete = models.SET_NULL, null=True)
-    school_class = models.ForeignKey(SchoolClass, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    user = OneToOneField(User, on_delete = models.CASCADE)
+    school_class=models.ForeignKey(SchoolClass, on_delete = models.SET_NULL, null=True)
+    school=models.ForeignKey(School, on_delete = models.SET_NULL, null=True)
+    subject=models.ManyToManyField(SchoolSubject)
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+
+
+
+
+
+
+
+
+
 
