414045374e437340edf9e2b72d4e6347e13bd2c2
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-06 23:37:49 +0200

    TestListSchoolTeachers test added

--- school/tests/test_views.py
+++ school/tests/test_views.py
@@ -17,3 +17,30 @@ class TestListSchool(APITestCase):
         self.assertEqual(result[0]['name'], 'School1')
 
 
+# Teacher.objects.filter(school__id = pk)
+class TestListSchoolTeachers(APITestCase):
+    pk_url = '1'
+    url = f'/api/schools/{pk_url}/teachers/'
+    url = '/api/schools/1/teachers/'
+    
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user('username6', 'Pas$w0rd')
+        self.client.force_authenticate(self.user)
+    
+    def test_teachers_list_GET(self):
+        
+        school = School.objects.create(name='School1')
+        
+        teacher = Teacher.objects.create( first_name="John",
+last_name="Smith",user=self.user)
+        teacher.school.add(school)
+        response = self.client.get(self.url)
+        result = response.json()
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result[0]['first_name'], 'John')
+        self.assertEqual(result[0]['last_name'], 'Smith')
+        self.assertEqual(result[0]['user'], self.user.id)
